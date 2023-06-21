e263fa5d9abe2452909cc68e93f06df915adbba8
arielcollatti <colatti2017@gmail.com>
2023-06-21 15:32:21 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine(" ---DiabetesGestacional--- \n");
+
+Console.Write("Digite o valor de glicemia (mg/dL)...: ");
+double glicemia = double.Parse(Console.ReadLine()!);
+
+if (glicemia < 92 )
+{
+    Console.Write("Reavaliar entre 24 e 28 semanas com TTOG 75g de glicose");
+}
+    else if  (glicemia >= 92 && glicemia < 126)
+    {
+           Console.Write("Comapatível com Diabettes Gestacional");
+    }
+else
+{
+    Console.Write("Diabetes Mellitus na Gravizez");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
